d5662928c2d6544a818f5a4fa32c8b68cd4410b8
Matt <mdwade@gmail.com>
2019-03-26 23:47:33 -0500

    Added form patials and destroy action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -43,6 +43,13 @@ class ArticlesController < ApplicationController
     end
   end
 
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    flash[:notice] = "Article has been deleted. (╯°□°)╯︵ ┻━┻"
+    redirect_to articles_path
+  end
+
 
 
   private
